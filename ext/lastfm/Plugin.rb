ff7d522b0c04b812b57d060b7755723b5edcee1b
Robin <rdex@external.(none)>
2014-02-12 14:03:55 +0100

    implemented lastfm plugin

--- ext/lastfm/Plugin.rb
+++ ext/lastfm/Plugin.rb
@@ -1,41 +1,53 @@
-require 'app/plugins/PluginFrame'
-
 require 'lastfm'
 
-class LastFm < PluginFrame
+require 'app/plugins/PluginFrame'
+
+# Plugin to aggregate LastFM data
+class LastfmPlugin < PluginFrame
+
 	def initialize(plugin_model)
 		super plugin_model
 
 		@lastfm = Lastfm.new(setting.consumer_key, setting.consumer_secret)
-
-		if setting.consumer_token.empty?
-			token = @lastfm.auth.get_token
-			logger.warn "Grant api access via http://www.last.fm/api/auth?api_key=#{setting.consumer_key}&token=#{token}."
-			logger.warn "Save the token key \"#{token}\" to the settings file!"
-		end
-		if !setting.consumer_token.empty? && setting.consumer_session.empty?
-			begin
-				@lastfm.session = @lastfm.auth.get_session(:token => setting.consumer_token)['key']
-			rescue => e
-				logger.warn "Grant api access via http://www.last.fm/api/auth?api_key=#{setting.consumer_key}&token=#{setting.consumer_token}."
-			end
-
-			logger.warn "Save the session key \"#{@lastfm.session}\" to the settings file!"
-		end
+		@lastfm.session = @lastfm.auth.get_session(token: setting.consumer_token)
 	end
 
 	def run
-		aggregate_weekly_summary
+		followers_action = get_action('lastfm_follower_aggregation')
+		if action_ready?(followers_action, setting.sleep_timer)
+			aggregate_followers(followers_action)
+		end
 	end
 
 	private
 
-	def aggregate_weekly_summary
-		name = 'weekly_summary'
-		if AggregateAction.get_time_since_last_execution(name, @plugin) < 1.week
-			logger.debug "Aggregate weekly summary"
-			@lastfm.user.get_loved_tracks(:user => setting.username)
+	# Aggregate followers
+	def aggregate_followers(action)
+		Log.new(loggable: action, title: "Aggregating LastFM followers.").save!
+		
+		followers = []
+
+		@lastfm.user.get_friends(user: @lastfm.session['name']).each do |f|
+			follower = Follower.find_or_initialize_by(username: f['id'].to_s)
+			follower.name = f['name']
+			follower.profile_url = f['url']
+			follower.image_url = f['image'][2]['content']
+			follower.action = action
+
+			if follower.changed?
+				followers << follower
+				logger.debug "Follower is new or changed. It will be persisted soon."
+			end
 		end
-		nil
+
+		# Persist/update followers
+		if followers.count > 0
+			Follower.transaction do
+				followers.each do |f|
+					f.save!
+					logger.info "Persisted new/updated follower (#{f.name})."
+				end
+			end
+		end
 	end
 end
